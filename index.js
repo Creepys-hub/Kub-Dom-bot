4bfe073b6abb9ccf666c6a47769925d4d77c49e6
Creepys-hub <52254658+Creepys-hub@users.noreply.github.com>
2019-07-08 20:33:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ bot.on('message', async message => {
 		console.log(`${message.author},`,"demande de l'aide");
 /* const channels = bot.channels.find(ch => ch.name === '🚫šтαff🛠');
   channels.send(`Le joueur ${member} demande de l'aide merci @staff de l'aider`);*/
-var channelst = bot.channels.find("name", "🚫šтαff🛠");
+var channelst = bot.channels.find("name", "🚫šтαff");
 channelst.send(`${message.author},`,"demande de l'aide");
 }
 
