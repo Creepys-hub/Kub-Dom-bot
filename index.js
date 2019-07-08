a1721bf9850645ae04fb2959ad61ca9987c441bb
Creepys-hub <52254658+Creepys-hub@users.noreply.github.com>
2019-07-08 22:13:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const cfg = process.env.token;
-const prefix = ("? ");
-
-bot.on('ready', function () {
-    console.log("Oui jvais bossé patron xD")
-    bot.user.setActivity('? help').catch(console.error)
-});
-
-bot.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const msg = member.guild.channels.find(ch => ch.name === 'ⴆιҽɳʋҽɳυҽ');
-  // Do nothing if the channel wasn't found on this server
-  if (!msg) return;
-  // Send the message, mentioning the member
-  msg.send(`Bienvenue sur le serveur Officiel du serveur Kub-Dom je t'invite à utiliser la commande ? help pour avoir des informations et de regarder le réglement du serveur merci et bon jeu à toi sur le serveur :video_game:  , ${member}`);
-});
-
-bot.on('message', async message => {
-	//message pour le Quizz
-	if(message.content === prefix + "Quizz") {
-	   message.channel.send(`pas encore codé ${message.author}`);
+    const Discord = require('discord.js');
+    const bot = new Discord.Client();
+    const cfg = require('./index.json');
+    const prefix = ("Cp ");
+    
+    bot.on('ready', function () {
+        console.log("Oui jvais bossé patron xD")
+        bot.user.setActivity('? help').catch(console.error)
+    });
+    
+    bot.on('guildMemberAdd', member => {
+      // Send the message to a designated channel on a server:
+      const msg = member.guild.channels.find(ch => ch.name === 'ⴆιҽɳʋҽɳυҽ');
+      // Do nothing if the channel wasn't found on this server
+      if (!msg) return;
+      // Send the message, mentioning the member
+      msg.send(`Bienvenue sur le serveur Officiel du serveur Kub-Dom je t'invite à utiliser la commande ? help pour avoir des informations et de regarder le réglement du serveur merci et bon jeu à toi sur le serveur :video_game:  , ${member}`);
+    });
+        
+    bot.on('message', async message => {
+    	//help du staff envers un joueur
+ 	if(message.content.startsWith(prefix + "helpmod")) {	
+	let myRole = message.guild.roles.find(role => role.name === "Testeur");
+	var chm = bot.channels.find("name", "🚫šтαff🛠");
+	chm.send(`${message.author},demande de l'aide merci de l'aider `+myRole);
     }
-	//Help de kubdom
-if(message.content === prefix + "Info") {
-      var aide_embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setTitle(`:robot: Informations sur le serveur Kub-Dom`)
-      .setDescription(`Voici mes commandes disponible :`)
-      .setThumbnail(message.author.avatarURL)
-      .addField("? Ip" ,"Pas encore disponible")
-      .addField("? Web" ,"lien du site OFFICIEL du serveur <3 ")
-      .addField("? Serveur" ,"Etat du serveur")
-      .setFooter("Menu Info bot - By Creep_1")
-      .setTimestamp()
-      message.channel.send(aide_embed);
-}
-	if(message.content.startsWith(prefix + "Ip")){
-	message.channel.send(`On a dit quoi c'est n'est pas encore disponible`);
-	}
-	
-	if(message.content.startsWith(prefix + "Web")){
-	message.channel.send(`le lien -> https://kubdom.000webhostapp.com`);
-	}
-	
-	if(message.content.startsWith(prefix + "Serveur")){
-	message.channel.send(`Le Serveur est fermé pour le moment désolé on finit bientôt :kissing_heart: `);
-	}
-	
-		//help du staff envers un joueur
- 	//if(message.content.startsWith(prefix + "helpmod")) {	
-		//let myRole = message.guild.roles.find(role => role.name === "Testeur");
-		//var chm = bot.channels.find("name", "🚫šтαff🛠");
-		//chm.send(`${message.author},demande de l'aide merci de l'aider `+myRole);
-//}
-	
+        //Help de kubdom
+    if(message.content === prefix + "Info") {
+          var aide_embed = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .setTitle(`:robot: Informations sur le serveur Kub-Dom`)
+          .setDescription(`Voici mes commandes disponible :`)
+          .setThumbnail(message.author.avatarURL)
+          .addField("Cp Ip" ,"Pas encore disponible")
+          .addField("Cp Web" ,"lien du site OFFICIEL du serveur <3 ")
+          .addField("Cp Serveur" ,"Etat du serveur")
+          .setFooter("Menu Info bot - By Creep_1")
+          .setTimestamp()
+          message.channel.send(aide_embed);
+    }
+        if(message.content.startsWith(prefix + "Ip")){
+        message.channel.send(`On a dit quoi c'est n'est pas encore disponible`);
+        }
+        
+        if(message.content.startsWith(prefix + "Web")){
+        message.channel.send(`le lien -> https://kubdom.000webhostapp.com`);
+        }
+        
+        if(message.content.startsWith(prefix + "Serveur")){
+        message.channel.send(`Le Serveur est fermé pour le moment désolé on finit bientôt :kissing_heart: `);
+        }
+    if (message.content.startsWith(prefix + "clear")) {
+            if (message.member.hasPermission("MANAGE_MESSAGES")) {
+                message.channel.fetchMessages()
+                   .then(function(list){
+                        message.channel.bulkDelete(list);
+                    }, function(err){message.channel.send("Pas de assez de permission pour executer la commande")})                        
+            }
+        }
     //message pour afficher le help   
     if(message.content === prefix + "help") {
       var aide_embed = new Discord.RichEmbed()
