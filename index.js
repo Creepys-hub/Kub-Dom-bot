9f4650db70ff019689b2c722c8843e315ce8c78c
Creepys-hub <52254658+Creepys-hub@users.noreply.github.com>
2019-07-08 22:29:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const prefix = ("? ");
     
     bot.on('guildMemberAdd', member => {
       // Send the message to a designated channel on a server:
-      const channel = member.guild.channels.find(ch => ch.name === 'ⴆιҽɳʋҽɳυҽ');
+      const channel = member.guild.channels.find(ch => ch.name === 'bvn');
       // Do nothing if the channel wasn't found on this server
       if (!channel) return;
       // Send the message, mentioning the member
